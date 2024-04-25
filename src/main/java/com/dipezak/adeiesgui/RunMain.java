57d2fe0191e757e6098194bd0cb310c47fceb4a0
persidis <gr_timair@yahoo.com>
2024-04-25 12:06:59 +0300

    Add files via upload

---
+++ src/main/java/com/dipezak/adeiesgui/RunMain.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dipezak.adeiesgui;
+
+/**
+ *
+ * @author user
+ */
+public class RunMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App.main(args);
+    }
+}
